ba1a9a00918b4914898ca88c06c6b3e2af408d55
millimo <millimoji@gmail.com>
2022-02-06 13:48:47 +0900

    adjust prob?

--- tools/script/mecabtest.cmd
+++ tools/script/mecabtest.cmd
@@ -1,3 +1,5 @@
+call mkmecab.cmd
+
 set MECAB="c:\Program Files (x86)\MeCab\bin\mecab.exe"
 
 %MECAB% --input-buffer-size=32768 --output=output.txt input.txt
--- tools/script/mkmecab.cmd
+++ tools/script/mkmecab.cmd
@@ -1,10 +1,13 @@
 @rem this script should run with Admin
+setlocal
 pushd
 
 copy /y user.csv "c:\Program Files (x86)\MeCab\dic\ipadic"
+copy /y overwrite.csv "c:\Program Files (x86)\MeCab\dic\ipadic"
 
 cd /d  "c:\Program Files (x86)\MeCab\dic\ipadic"
 
 ..\..\bin\mecab-dict-index.exe -f SHIFT-JIS -t UTF-8
 
 popd
+endlocal
