6654ca599eab6e9560b290d16276bf0ff2403580
Shinji Morimitsu <morimitu@gmail.com>
2022-03-07 00:04:25 +0900

    add runloop

--- tools/script/runcrawlerloop.cmd
+++ tools/script/runcrawlerloop.cmd
@@ -4,10 +4,15 @@ set WEBCRAWLEREXE=%SCRIPTDIR%..\WebCrawler\bin\Debug\WebCrawler.exe
 
 :LOOPTOP
 
-%WEBCRAWLEREXE%
-
-timeout /T 60
+git pull
 
 call %SCRIPTDIR%mkmecab.cmd
+
+msbuild.exe %SCRIPTDIR%..\RibbonTools.sln /t:rebuild /p:Configuration=Debug /p:Platform="Any CPU"
+
+%WEBCRAWLEREXE%
+
+@echo ====================== cool down ==============================
+timeout /T 60
 
 goto :LOOPTOP
