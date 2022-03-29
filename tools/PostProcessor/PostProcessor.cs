57eb1f1ac28c5b43c5a2b8c11f3c78654bf9c97e
millimo <millimoji@gmail.com>
2022-03-29 16:07:08 +0900

    analyze 2 files

--- tools/PostProcessor/PostProcessor.cs
+++ tools/PostProcessor/PostProcessor.cs
@@ -46,7 +46,7 @@ namespace Ribbon.PostProcessor
             long stateCount = 0;
             try
             {
-                var sourceFiles = Shared.CompressedFileSet.getNewerFiles(1);
+                var sourceFiles = Shared.CompressedFileSet.getNewerFiles(2);
                 foreach (var sourceFile in sourceFiles)
                 {
                     using (var decompressor = new Shared.CompressedStreamReader(sourceFile))
